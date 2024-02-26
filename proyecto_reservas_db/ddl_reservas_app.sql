d6d9780ce3e9ed30fdb911c3a107316ba4b549cd
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-02-26 21:17:51 +0000

    clase 10

--- proyecto_reservas_db/ddl_reservas_app.sql
+++ proyecto_reservas_db/ddl_reservas_app.sql
@@ -1,6 +1,28 @@
 -- CREACION BASE DE UNA APP PARA RESERVAS UN RESTAURANT USANDO DISTINTOS
 
 /*
+### Problema:
+
+Nuestro equipo de desarrollo está trabajando en un sistema de gestión de reservas para restaurantes, y nos enfrentamos a la necesidad de diseñar una base de datos eficiente que pueda manejar todas las operaciones relacionadas con las reservas de manera óptima.
+
+### Descripción del Problema:
+
+1. **Gestión de Clientes y Empleados**: Necesitamos una base de datos que nos permita registrar la información de los clientes que realizan reservas, así como de los empleados involucrados en el proceso de reserva, como los camareros o encargados de atención al cliente.
+
+2. **Gestión de Tipos de Reserva**: Es importante poder clasificar las reservas según su tipo, ya sea una reserva estándar, una reserva para eventos especiales o reservas de grupos grandes. Esto nos ayudará a organizar mejor el flujo de trabajo y adaptar nuestros servicios según las necesidades del cliente.
+
+3. **Gestión de Mesas y Disponibilidad**: La base de datos debe permitirnos registrar la disponibilidad de mesas en cada restaurante, así como gestionar su capacidad y estado (ocupado o disponible). Esto es fundamental para garantizar una asignación eficiente de mesas y evitar conflictos de reservas.
+
+4. **Registro de Reservas**: Necesitamos un sistema que pueda registrar de manera detallada cada reserva realizada, incluyendo la fecha y hora de la reserva, el cliente que la realizó, la mesa reservada, el empleado que atendió la reserva y el tipo de reserva.
+
+### Objetivo:
+
+Diseñar e implementar una base de datos relacional que satisfaga todas las necesidades de gestión de reservas para nuestro sistema de gestión de restaurantes. Esta base de datos deberá ser eficiente, escalable y fácil de mantener, permitiendo una gestión ágil y precisa de todas las operaciones relacionadas con las reservas.
+
+*/
+
+
+/*
 
 ## Descripción de la Base de Datos - Gestión de Reservas en Restaurantes
 
--- scripts/clase11.sql
+++ scripts/clase11.sql
@@ -48,4 +48,58 @@ Estos son los mismos ejemplos adaptados para MySQL, que es una implementación c
 
 
 
+-- IE 
+-- INSERT RECORDS
+    -- INSERCION FULL COLUMNAS
+        
+    -- INSERCION PARCIAL
+
+
+-- UPDATE RECORDS
+    -- ACTUALIZADO UNICO
+    -- ACTUALIZADO MASIVO
+
+
+-- ejemplo de la filmina : 
+-- Si deseamos actualizar los niveles de las clases de juegos, pasar a nivel 8 todas las clases que están entre la 1 y la 20 inclusive  y cuyos niveles actuales están por debajo del 13
+
+SELECT
+ *
+FROM GAME
+WHERE TRUE
+    AND id_class BETWEEN 1  AND 20
+    AND id_level < 13
+;
+
+
+-- ¿Cuántos registros se actualizarían y Cuál sería la cláusula UPDATE?
+
+SELECT * 
+FROM GAME
+WHERE id_game IN (55,57,8,93,23);
+
+-- EN UN PRINCIPIO NO SE PODRIA POR UN TEMA DE INTEGRIDAD 
+-- REFERENCIAL
+
+UPDATE GAME
+    SET id_level = 8
+WHERE TRUE
+AND id_class BETWEEN 1 AND 20
+AND id_level < 13 ; 
+
+-- DESABILITANDO LA OPCION DE INTEGRIDAD
+SET FOREIGN_KEY_CHECKS=0;
+-- AL CORRER NUEVAMENTE LA QUERY SE ACTUALIZARA UNA CANTIDAD DETERMINADA
+
+-- DELETE | LOGICAL DELETE
+
+
+-- Bibliografia adicional 
+-- https://www.linkedin.com/pulse/sargable-vs-non-query-md-mehedi-hasan/
+
+SARGABLE QUERIES
+
+- Avoid using functions or calculations on indexed columns in the WHERE clause.
+- Use direct comparisons when possible, instead of wrapping the column in a function.
+- If we need to use a function on a column, consider creating a computed column or a function-based index, if the database system supports it.
 
